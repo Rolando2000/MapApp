827e5a455a13a2ef54ada81771a20399c1d68b71
Rolando2000 <61158161+Rolando2000@users.noreply.github.com>
2021-04-26 19:36:48 +0100

    api da google maps a funcionar

--- app/src/main/java/ipvc/estg/mapapp/api/EndPoints.kt
+++ app/src/main/java/ipvc/estg/mapapp/api/EndPoints.kt
@@ -11,6 +11,11 @@ interface EndPoints {
     @GET("/myslim/api/user/{id}")
     fun getUserById(@Path("id") id:Int): Call<User>
 
+    @FormUrlEncoded
+    @POST("/myslim/api/user/login")
+    fun login(@Field("username") username:String?,
+              @Field("password") password:String?): Call<OutputPost>
+
 
 
 }
--- app/src/main/java/ipvc/estg/mapapp/retrofitLogin.kt
+++ app/src/main/java/ipvc/estg/mapapp/retrofitLogin.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import ipvc.estg.mapapp.api.EndPoints
+import ipvc.estg.mapapp.api.OutputPost
 import ipvc.estg.mapapp.api.ServiceBuilder
 import ipvc.estg.mapapp.api.User
 import retrofit2.Call
@@ -20,63 +21,51 @@ class retrofitLogin : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_retrofit_login)
 
-        val request = ServiceBuilder.buildService(EndPoints::class.java)
-        val call = request.getUsers()
-
         val userText = findViewById<TextView>(R.id.user)
         val passText = findViewById<TextView>(R.id.pass)
 
         val teste=findViewById<TextView>(R.id.teste)
 
-        call.enqueue(object : Callback<List<User>> {
-            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+        val login = findViewById<Button>(R.id.btn_login)
+        login.setOnClickListener {
 
-                if (response.isSuccessful) {
+            val request = ServiceBuilder.buildService(EndPoints::class.java)
 
-                    val i: List<User> = response.body()!!
+            val user = userText.text.toString()
+            val pass = passText.text.toString()
 
-                    var a: Int = i.size
+            teste.text=user
 
-                    val button = findViewById<Button>(R.id.btn_login)
-                    button.setOnClickListener {
-                        var num: Int = 0
-                        var aux: Int = 0
-                        var auxPa: Int = 0
-                        val user = userText.text.toString()
-                        val pass = passText.text.toString()
-                        teste.text=user
-                        do {
-                            if (i[num].user == user) {
-                                aux = 1
+            if(user == "") {
+                Toast.makeText(
+                        applicationContext,
+                        getString(R.string.username_null),
+                        Toast.LENGTH_LONG).show()
+            }
+            else if(pass == "") {
+                Toast.makeText(
+                        applicationContext,
+                        getString(R.string.pass_null),
+                        Toast.LENGTH_LONG).show()
+            }
+            else {
+                val call = request.login(user, pass)
 
-                                if (i[num].pass == pass) {
-                                    auxPa = 1
-                                    teste.text="login feito"
-                                }
-                            }
-                            num++;
-                        } while (num < a)
+                call.enqueue(object : Callback<OutputPost> {
+                    override fun onResponse(call: Call<OutputPost>, response: Response<OutputPost>) {
+                        if (response.isSuccessful) {
 
-                        if(aux == 0) {
-                            Toast.makeText(
-                                applicationContext,
-                                getString(R.string.WrongUser),
-                                Toast.LENGTH_SHORT).show()
-
+                            val c: OutputPost = response.body()!!
+                            Toast.makeText(this@retrofitLogin,c.MSG,Toast.LENGTH_SHORT).show()
                         }
-                        if(auxPa == 0) {
-                            Toast.makeText(
-                                applicationContext,
-                                getString(R.string.wrongPass),
-                                Toast.LENGTH_SHORT).show()
-                        }
-
                     }
-                }
+
+                    override fun onFailure(call: Call<OutputPost>, t: Throwable) {
+                        Toast.makeText(this@retrofitLogin, "${t.message}", Toast.LENGTH_SHORT).show()
+                    }
+
+                })
             }
-            override fun onFailure(call: Call<List<User>>, t: Throwable) {
-                Toast.makeText(this@retrofitLogin, "${t.message}", Toast.LENGTH_SHORT).show()
-            }
-        })
+        }
     }
 }
